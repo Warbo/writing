2c2c1b21404d121ced883dc2fcce4f2244c44a52
Chris Warburton <chriswarbo@gmail.com>
2015-02-12 13:16:51 +0000

    Use graphs and stuff

--- default.nix
+++ default.nix
@@ -7,5 +7,14 @@ stdenv.mkDerivation {
     pkgs.texLiveFull # LaTeX
     pkgs.emacs
     pkgs.ghostscript
+    pkgs.graphviz
+    pkgs.blockdiag
+    pkgs.haskellPackages.pandoc
+    pkgs.haskellPackages.pandocCiteproc
+    panpipe
+    panhandle
+    coq_mtac
+    git
+    treefeatures
   ];
 }
