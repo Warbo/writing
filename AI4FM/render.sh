92c31abbca47225ab1cfd0b2c9a5ad6e1c4d107e
Chris Warburton <chriswarbo@gmail.com>
2015-08-06 11:26:12 +0100

    Added render

--- AI4FM/render.sh
+++ AI4FM/render.sh
@@ -1,9 +1,25 @@
 #! /usr/bin/env nix-shell
 #! nix-shell -i bash -p bash -p md2pdf
 
-function go {
+function renderMd {
     ARGS="--filter pandoc-citeproc" md2pdf
 }
+
+function mdToLatex {
+    pandoc -s --filter pandoc-citeproc -o abstract.tex abstract.md
+}
+
+function renderLatex {
+    latex abstract
+    latex abstract
+    pdflatex abstract
+}
+
+function go {
+    renderLatex
+}
+
+# Run once (default) or forever (if given any argument)
 
 go
 
