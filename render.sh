4bd0bbe5df171a628ec8116e0c82513afb7b602d
Chris Warburton <chriswarbo@gmail.com>
2015-08-19 14:19:32 +0100

    Added more formats to render.sh

--- render.sh
+++ render.sh
@@ -1,23 +1,56 @@
 #! /usr/bin/env nix-shell
 #! nix-shell -p bash -p pandoc -i bash
 
-echo "Checking contents of stp/"
-while IFS= read -r -d $'\0' FILE
-do
-    echo "Do not know how to process $FILE, aborting"
+FORMATS="png jpg ico"
+
+function check {
+    # Directories are OK
+    [[ -d "$1" ]] && return 0
+
+    # Check filename extension against ones we know how to process
+    EXT=$(echo "$1" | rev | cut -d '.' -f 1 | rev)
+    for KNOWN in md $FORMATS
+    do
+        [[ "x$EXT" = "x$KNOWN" ]] && return 0
+    done
+    echo "Don't know how to handle $1" >> /dev/stderr
     exit 1
-done < <( find stp/ -type f -not -name '*.md' -print0 )
-echo "OK"
+}
+
+function copy {
+    DIR=$(dirname "$1")
+    mkdir -p "rendered/$DIR"
+    cp -v "$1" "rendered/$DIR/"
+}
+
+function copyAll {
+    for FILE in stp/**/*."$1"
+    do
+        copy "$FILE"
+    done
+}
+
+echo "Checking contents of stp/"
+shopt -s globstar
+for file in stp/**/*
+do
+    check "$file"
+done
 
 echo "Removing old version"
 rm -r rendered/
 
 echo "Rendering all Markdown files to HTML"
-shopt -s globstar
 for REL in stp/**/*.md
 do
     DIR=$(dirname "$REL")
     NAME=$(basename "$REL" .md)
     mkdir -p "rendered/$DIR"
-    pandoc -i "$DIR/$NAME.md" -o "rendered/$DIR/$NAME.html"
+    pandoc -s -i "$REL" -o "rendered/$DIR/$NAME.html"
+done
+
+echo "Putting images in place"
+for FORMAT in $FORMATS
+do
+    copyAll "$FORMAT"
 done
