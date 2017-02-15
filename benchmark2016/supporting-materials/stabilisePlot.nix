787914f32f382736d5be2c747734f393e6f28cdc
Chris Warburton <chriswarbo@gmail.com>
2017-02-15 17:52:23 +0000

    Test plots before gathering data

--- benchmark2016/supporting-materials/stabilisePlot.nix
+++ benchmark2016/supporting-materials/stabilisePlot.nix
@@ -1,8 +1,13 @@
+with builtins;
 with rec {
   pkgs = import <nixpkgs> {};
 
   inherit (pkgs)
-    buildEnv jq makeWrapper pythonPackages runCommand stdenv writeScript;
+    buildEnv graphicsmagick jq lib makeWrapper pythonPackages runCommand stdenv
+    writeScript;
+
+  inherit (lib)
+    range stringToCharacters take toInt;
 
   haskell-te = import (pkgs.fetchFromGitHub {
     owner  = "Warbo";
@@ -12,10 +17,12 @@ with rec {
   });
 
   getData = cmd: pkgs.stdenv.mkDerivation {
+    # Forces tests to be run before we spend ages getting the data
+    inherit plotTests;
     name = "${cmd}-stabilise-data";
-    buildInputs  = [ haskell-te ];
+    buildInputs  = [ haskell-te jq ];
     buildCommand = ''
-      SAMPLE_SIZES="5 10 15" REPS=30 ${cmd} > "$out"
+      SAMPLE_SIZES="5 10 15" REPS=30 ${cmd} | jq -s '.' > "$out"
     '';
   };
 
@@ -34,42 +41,44 @@ with rec {
         import sys
         import matplotlib
         import numpy as np
-        matplotlib.use('Agg')
         import matplotlib.pyplot as plt
         import json
-        import io
         import random
-
-        data = json.loads(sys.stdin.read())
 
         def time_of(r):
           return r["time"] if r["failure"] is None else 0
 
-        def draw_fig(data, buf, title):
+        def draw_bar(data, mode, size):
           N = len(data)
           x = range(N)
 
           plt.figure(figsize=(8, 3))
           plt.bar(x, data, width=1/1.5, color="blue", log=True)
-          plt.title(title)
+          plt.title("Sample size %s, %s" % (size, mode))
 
-          plt.savefig(buf, format='png')
-          buf.seek(0)
-          return buf
+          plt.savefig("bar-%s-%s.png" % (mode, size))
 
-        def draw_figs(times, shuffled, index):
+        def draw_bars(times, shuffled, index):
           # Write the ordered times
-          draw_fig(times, ordered[index],
-                   "Ordered samples of size %s" % sizes[index])
+          draw_bar(times, "ordered", sizes[index])
 
           # Write the shuffled times
-          draw_fig(shuffled, scrambled[index],
-                   "Shuffled samples of size %s" % sizes[index])
+          draw_bar(shuffled, "shuffled", sizes[index])
+
+        def lag_plot(lag, times, mode, index):
+          plt.figure(figsize=(8, 8))
+          plt.plot(times[lag:], times[:-1*lag], 'kx')
+          plt.title("Lag %s, sample size %s, %s" % (lag, sizes[index], mode))
+
+          plt.savefig("lag-%s-%s-%s.png" % (lag, mode, sizes[index]))
+
+        def lag_plots(times, mode, index):
+          for lag in [1,2,3,4]:
+            lag_plot(lag, times, mode, index)
 
         # For each run of data, render an ordered figure and a shuffled figure
-        ordered   = [io.BytesIO() for x in data]
-        scrambled = [io.BytesIO() for x in data]
-        sizes     = [r["info"] for r in data]
+        data  = json.loads(sys.stdin.read())
+        sizes = [r["info"] for r in data]
         for index, run in enumerate(data):
           sample_size = int(run["info"])
           times       = map(time_of, run["results"])
@@ -78,36 +87,9 @@ with rec {
           shuffled = [x for x in times]
           random.Random(42).shuffle(shuffled)
 
-          draw_figs(times, shuffled, index)
-
-        # Combine plot images
-        from PIL import Image
-
-        def beside(im1, im2):
-          w1, h1 = im1.size
-          w2, h2 = im1.size
-          result = Image.new("RGB", (w1+w2, max(h1,h2)))
-          result.paste(im1, (0, 0))
-          result.paste(im2, (w1,0))
-          return result
-
-        def above(im1, im2):
-          w1, h1 = im1.size
-          w2, h2 = im1.size
-          result = Image.new("RGB", (max(w1,w2), h1+h2))
-          result.paste(im1, (0, 0))
-          result.paste(im2, (0,h1))
-          return result
-
-        ims    = map(lambda index: above(Image.open(ordered[index]),
-                                         Image.open(scrambled[index])),
-                     range(len(data)))
-        result = reduce(beside, ims)
-
-        map(lambda buf: buf.close(), ordered + scrambled)
-
-        # Write out combined image
-        result.save(sys.stdout, format="PNG")
+          draw_bars(times,    shuffled,   index)
+          lag_plots(times,    "ordered",  index)
+          lag_plots(shuffled, "shuffled", index)
       '';
       env = with pythonPackages; python.buildEnv.override rec {
         extraLibs = [ numpy matplotlib pillow scipy ];
@@ -117,27 +99,103 @@ with rec {
       makeWrapper "${plotter}" "$out" --prefix PATH : "${env}/bin"
     '';
 
-    chartOf = name: data:
+    barChartOf = name: data:
       stdenv.mkDerivation {
         name         = "barchart-${name}";
-        buildInputs  = [ jq ];
+        buildInputs  = [ graphicsmagick ];
         buildCommand = ''
-          jq -s '.' < "${data}" | "${barChart}" > "$out"
+          mkdir -p "$out"
+          cd "$out"
+          echo "Plotting bar charts for ${data}" 1>&2
+          "${barChart}" < "${data}"
+          for ORD in bar-ordered-*.png
+          do
+            SHUF=$(echo "$ORD" | sed -e 's/ordered/shuffled/g')
+            COMB=$(echo "$ORD" | sed -e 's/ordered/combined/g')
+            gm convert "$ORD" "$SHUF" -append "$COMB"
+          done
         '';
       };
+
+    plotsOf = name: data:
+      pkgs.runCommand "stabilisePlotsOf-${name}"
+        {
+          inherit data;
+          bars = barChartOf name data;
+        }
+        ''
+          mkdir "$out"
+          cp -rv "$data" "$out/data.json"
+          cp -rv "$bars" "$out/bars"
+        '';
+
+    plotTests = stdenv.mkDerivation {
+      name = "plot-tests";
+      # Generate some semi-plausible data to test with
+      src  =
+        with rec {
+          data = writeScript "test-data" (toJSON [corr uncorr]);
+          corr = {
+            info    = 1;
+            results = map (n: {
+                            time    = (2 * n) + 3;
+                            failure = if elem n [3 8]
+                                         then n + 20
+                                         else null;
+                          })
+                          (range 1 30);
+          };
+
+          uncorr = {
+            info    = 2;
+            results = map (n: {
+                            time    = randomise n;
+                            failure = if elem n [3 8]
+                                         then n + 20
+                                         else null;
+                          })
+                          (range 1 30);
+          };
+
+          randomise = n:
+            with rec {
+              hash   = hashString "sha256" (toString n);
+              digits = stringToCharacters "0123456789";
+              valid  = filter (c: elem c digits) (stringToCharacters hash);
+              prefix = take 4 (valid ++ digits);
+            };
+            toInt (concatStringsSep "" prefix);
+        };
+        plotsOf "test" data;
+
+      doCheck    = true;
+      checkPhase = ''
+        echo "Testing plots from $src" 1>&2
+        for MODE in ordered shuffled combined
+        do
+          for SET in $(seq 1 3)
+          do
+            [[ -f "$src/bars/bar-$MODE-$SET.png" ]] || {
+              echo "Missing bar chart $MODE $SET" 1>&2
+              exit 1
+            }
+          done
+        done
+      '';
+    };
 };
 
 pkgs.runCommand "stabilisePlot"
   {
-    quickSpecData  = quickSpec.data;
-       mlSpecData  = mlSpec.data;
-    quickSpecChart = chartOf "quickSpec" quickSpecData;
-       mlSpecChart = chartOf    "mlSpec"    mlSpecData;
+    quickSpecData = quickSpec.data;
+       mlSpecData = mlSpec.data;
+    quickSpecBars = barChartOf "quickSpec" quickSpec.data;
+       mlSpecBars = barChartOf    "mlSpec"    mlSpec.data;
   }
   ''
     mkdir "$out"
-    cp -r "$quickSpecData"  "$out/quickSpecData.json"
-    cp -r    "$mlSpecData"  "$out/mlSpecData.json"
-    cp -r "$quickSpecChart" "$out/quickSpecStability.png"
-    cp -r    "$mlSpecChart" "$out/mlSpecStability.png"
+    cp -r "$quickSpecData" "$out/quickSpecData.json"
+    cp -r    "$mlSpecData" "$out/mlSpecData.json"
+    cp -r "$quickSpecBars" "$out/quickSpecStability.png"
+    cp -r    "$mlSpecBars" "$out/mlSpecStability.png"
   ''
