bf6a1fd49b93240174fbbb591e30b343603ee070
Chris Warburton <chriswarbo@gmail.com>
2017-02-25 08:22:20 +0000

    Precision/recall for small theories

--- benchmark2016/supporting-materials/stabiliseUtils.nix
+++ benchmark2016/supporting-materials/stabiliseUtils.nix
@@ -1,6 +1,12 @@
 with builtins;
 rec {
-  pkgs = import <nixpkgs> {};
+  defPkgs = import <nixpkgs> {};
+  pkgs    = import (defPkgs.fetchFromGitHub {
+    owner  = "NixOS";
+    repo   = "nixpkgs";
+    rev    = "16.09";
+    sha256 = "1cx5cfsp4iiwq8921c15chn1mhjgzydvhdcmrvjmqzinxyz71bzh";
+  }) { config = {}; };
 
   inherit (pkgs)
     buildEnv callPackage jq lib makeWrapper pythonPackages runCommand stdenv
@@ -52,26 +58,75 @@ rec {
     export            REPS=30
   '';
 
-  smallTheoryData = cmd: precisionRecallOfData (stdenv.mkDerivation {
-    name         = "${cmd}-small-theory-data";
-    hte          = haskell-te-src;
-    SKIP_DATA    = getEnv "SKIP_DATA";
-    buildInputs  = [ haskell-te jq ];
-    buildCommand = ''
-      ${explorationOptions}
-      for B in "$hte"/benchmarks/*.smt2
-      do
-        NAME=$(basename "$B" .smt2)
-        mkdir -p "$out"
-        if [[ -n "$SKIP_DATA" ]]
-        then
-          echo '[{"cmd":"${cmd}","info":"","results":[]}]' > "$out/$NAME.json"
-        else
-          ${cmd} < "$B"| jq -s '.' > "$out/$NAME.json"
-        fi
-      done
-    '';
-  });
+  smallTheoryData = cmd:
+    with rec {
+      data = stdenv.mkDerivation {
+        name         = "${cmd}-small-theory-data";
+        hte          = haskell-te-src;
+        SKIP_DATA    = getEnv "SKIP_DATA";
+        buildInputs  = [ haskell-te jq ];
+        buildCommand = ''
+          ${explorationOptions}
+          for B in "$hte"/benchmarks/*.smt2
+          do
+            NAME=$(basename "$B" .smt2)
+            mkdir -p "$out"
+            if [[ -n "$SKIP_DATA" ]]
+            then
+              echo '[{"cmd":"${cmd}","info":"","results":[]}]' > "$out/$NAME.json"
+            else
+              ${cmd} < "$B"| jq -s '.' > "$out/$NAME.json"
+            fi
+          done
+        '';
+      };
+    };
+    stdenv.mkDerivation {
+      inherit data;
+      name         = "small-theory-precision-recall";
+      buildInputs  = [ haskell-te-defs.tipBenchmarks.tools jq ];
+      buildCommand = ''
+        function stripMsgs {
+          grep -v "^Depth" < "$1" || echo ""
+        }
+
+        for F in "$data"/*.json
+        do
+          mkdir -p "$out"
+          NAME=$(basename "$F" .json)
+
+          echo "Looking for ground truth equations for '$NAME'" 1>&2
+          EQS_FILE="${./isabelle-theorems}/$NAME.smt2"
+          [[ -f "$EQS_FILE" ]] || {
+            echo "No such file '$EQS_FILE'" 1>&2
+            exit 1
+          }
+
+          echo "Looping through benchmark executions" 1>&2
+          while read -r EXECUTION
+          do
+            echo "Looping through benchmark iterations" 1>&2
+            while read -r ITERATION
+            do
+              echo "Extracting equations found by this iteration" 1>&2
+              STDOUT=$(echo "$ITERATION" | jq -r '.stdout')
+              EQUATIONS=$(stripMsgs "$STDOUT" | jq -s '.')
+
+              echo "Comparing equations to ground truth" 1>&2
+              WANTED=$(echo "$EQUATIONS" |
+                       GROUND_TRUTH="$EQS_FILE" \
+                       TRUTH_SOURCE="$EQS_FILE" precision_recall_eqs)
+
+              echo "Annotating the iteration with these results" 1>&2
+              echo "$ITERATION" | jq --argfile wanted <(echo "$WANTED") \
+                                     '. + $wanted'
+            done < <(echo "$EXECUTION" | jq -c '.results | .[]') |
+              jq -s --argfile execution <(echo "$EXECUTION") \
+                 '$execution + {"results": .}'
+          done < <(jq -c '.[]' < "$F") | jq -s '.' > "$out/$NAME.json"
+        done
+      '';
+    };
 
   precisionRecallOfData = data:
     stdenv.mkDerivation {
@@ -87,7 +142,6 @@ rec {
         function processFile {
           while read -r EXECUTION
           do
-            echo "EXECUTION: $EXECUTION" 1>&2
             SAMPLE_SIZE=$(echo "$EXECUTION" | jq -r '.info')
             echo "Analysing samples of size $SAMPLE_SIZE" 1>&2
             REP=0
