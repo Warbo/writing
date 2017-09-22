accf348aeb4e764228ec44c398312685f576e3ef
Chris Warburton <chriswarbo@gmail.com>
2017-09-22 12:50:24 +0100

    Fix AI4FM

--- AI4FM/default.nix
+++ AI4FM/default.nix
@@ -1,32 +1,31 @@
-with { inherit (import ../resources) nixpkgs; };
-with nixpkgs.repo1703."de1ceec";
-
-runCommand "ai4fm.pdf"
+with { inherit (import ../resources) bibtex nixpkgs; };
+with nixpkgs.repo1609."00ef7f9";
+with rec {
+  # Take TeX from 1703
+  tex = with nixpkgs.repo1703."de1ceec"; texlive.combine {
+    inherit (texlive)
+      beamer collection-basic collection-genericrecommended collection-latex
+      collection-latexrecommended enumitem;
+  };
+};
+runCommand "ai4fm"
   {
+    inherit bibtex;
     src         = ./.;
-    buildInputs = [
-      (texlive.combine {
-        inherit (texlive)
-          beamer collection-basic collection-bibtexextra collection-binextra
-          collection-context collection-fontsextra collection-fontsrecommended
-          collection-fontutils collection-formatsextra collection-genericextra
-          collection-genericrecommended collection-latex collection-latexextra
-          collection-latexrecommended collection-mathextra collection-plainextra
-          collection-pstricks collection-science; })
-      gnumake bash haskellPackages.pandoc-citeproc panpipe panhandle ditaa
-    ];
+    buildInputs = [ ditaa pandocPkgs tex ];
   }
   ''
     set -e
 
     # Slides
     pandoc -w dzslides --standalone --self-contained --mathml \
-           -o "$PWD/slides.html --filter=pandoc-citeproc --filter=panpipe \
+           -o "$PWD/slides.html" --filter=pandoc-citeproc --filter=panpipe \
            --filter=panhandle "$src/slides.md"
 
     # Article
     cp -r "$src" ./src
-    chmod +w -R ./src
+    chmod +w -R  ./src
+    cp "$bibtex"   src/Bibtex.bib
     pushd src
       latex article
       bibtex article
