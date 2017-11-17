7b0132ed2f37bec98b74795544c638e07388c204
Chris Warburton <chriswarbo@gmail.com>
2017-11-17 10:37:01 +0000

    Extract data with Python, skip duplicate samples

--- TEBenchmarkPaper/supporting-materials/graphs.nix
+++ TEBenchmarkPaper/supporting-materials/graphs.nix
@@ -1,4 +1,5 @@
-{ fail, fetchgit, gnuplot, jq, miller, mkBin, runCommand, wrap, writeScript }:
+{ fail, fetchgit, gnuplot, jq, miller, mkBin, python, runCommand, wrap,
+  writeScript }:
 
 with builtins;
 with rec {
@@ -107,37 +108,71 @@ with rec {
 
   quickspecData = runCommand "quickspecData"
     {
-      buildInputs = [ jq ];
-      zipped      = data;
+      script = wrap {
+        name   = "getQuickSpecData.py";
+        paths  = [ python ];
+        vars   = { zipped = data; };
+        script = ''
+          #!/usr/bin/env python
+          import os
+          import sys
+          msg = sys.stderr.write
+          get = lambda x, *path: x if path == () else get(x[path[0]], *path[1:])
+          num = lambda x: type(x) in [type(42), type(42.0)]
+
+          msg('Gathering input\n')
+          import gzip
+          import json
+          with gzip.open(os.getenv('zipped'), 'r') as zipped:
+            data = get(json.load(zipped),
+                       'results', 'quickspectip.track_data', 'result', 0)
+
+          msg('Setting up output\n')
+          out = os.getenv('out')
+          prec, rec, time = [out + '/' + x for x in ['prec', 'rec', 'time']]
+          map(os.mkdir, [out, prec, rec, time])
+          del out
+
+          for size in data:
+            msg('Extracting data for size {0}\n'.format(size))
+            sdata   = data[size]['reps']
+            samples = []
+            with open(prec + '/' + size, 'w') as precs, \
+                 open(rec  + '/' + size, 'w') as recs,  \
+                 open(time + '/' + size, 'w') as times:
+              for rep in sorted(sdata.keys()):
+                rdata = sdata[rep]
+
+                sample = sorted(rdata['sample'])
+                if sample in samples:
+                  msg('Size {0} rep {1} is a dupe, skipping\n'.format(
+                    size, rep))
+                else:
+                  samples += [sample]
+
+                  # Get the time if available, otherwise treat as a timeout
+                  # We use min since killing might take some time
+                  t = min(rdata['time'], rdata['timeout'])
+                  assert num(t), 'Non-numeric time %r' % type(t)
+                  times.write(str(t) + '\n')
+
+                  # Assume precision is 0 if not found
+                  p = rdata['precision'] if 'precision' in rdata else 0
+                  p = p or 0
+                  assert num(p), 'Non-numeric prec %r' % type(p)
+                  precs.write(str(p) + '\n')
+
+                  # Assume recall is 0 if not found
+                  r = rdata['recall'] if 'recall' in rdata else 0
+                  r = r or 0
+                  assert num(r), 'Non-numeric rec %r' % type(r)
+                  recs.write(str(r) + '\n')
+        '';
+      };
     }
     ''
-      #!/usr/bin/env bash
       set -e
-      PRE='.results | ."quickspectip.track_data" | .result | .[0]'
-      gunzip < "$zipped" > ./data
-
-      mkdir -p "$out/time"
-      mkdir -p "$out/prec"
-      mkdir -p "$out/rec"
-
-      jq -r "$PRE | keys | .[]" < ./data | while read -r SIZE
-      do
-        echo "Extracting data for size $SIZE" 1>&2
-        PRE2="$PRE | .[\"$SIZE\"] | .reps"
-        jq -r "$PRE2 | .[] | .time" < ./data > "$out/time/$SIZE"
-
-        jq -r "$PRE2 | .[] | .precision |
-               if . == null
-                  then 0
-                  else if . == []
-                          then 0
-                          else .
-                       end
-               end" < ./data > "$out/prec/$SIZE"
-
-        jq -r "$PRE2 | .[] | .recall |
-               if . == null then 0 else . end" < ./data > "$out/rec/$SIZE"
-      done
+      "$script" 1>&2
     '';
 };
 quickspecGraphs
