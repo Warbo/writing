39d5fec2a05b3c662ad542834c2de9449c04b706
Chris Warburton <chriswarbo@gmail.com>
2017-11-30 12:20:18 +0000

    Alphabetical

--- TEBenchmarkPaper/supporting-materials/default.nix
+++ TEBenchmarkPaper/supporting-materials/default.nix
@@ -15,44 +15,7 @@ rec {
   # http://www.springer.com/cda/content/document/cda_downloaddocument/?SGWID=0-0-45-468198-0
   latex = ./LaTeX.zip;
 
-  # Provides a pdflatex binary with all packages needed by template, our
-  # document and the matplotlib graphs
-  tex = (texlive.combine {
-    inherit (texlive)
-    scheme-small tikzinclude tikz-qtree algorithmicx algorithm2e algorithms
-    frankenstein csquotes multirow type1cm;
-  });
-
-  # Render a "dummy" version of the paper which has all of the same styling, but
-  # just spits out the text width to stdout. We can use this to generate figures
-  # of the correct size, without having to scale things up or down.
-  textWidth = render {
-    article = writeScript "article-width.tex" (import (runCommand "widthTex.nix"
-      {
-        inherit article;
-        buildInputs = [ jq ];
-        WIDTH       = ''\typeout{WIDTH \the\textwidth WIDTH}'';
-      }
-      ''
-        PREAMBLE=$(grep -B 1000 '\\begin{document}' < "$article")
-
-        echo "$PREAMBLE"      >  ./article.tex
-        echo "$WIDTH"         >> ./article.tex
-        echo '\end{document}' >> ./article.tex
-
-        jq -Rs '.' < ./article.tex > "$out"
-      ''));
-    graphs  = null;
-    name    = "test-width";
-    script  = ''
-      set -o pipefail
-      render | tee ./output
-      grep 'WIDTH[ ]*[0-9.]*pt[ ]*WIDTH' < ./output |
-      sed -e 's/WIDTH//g'                           |
-      tr -d 'pt ' > "$out"
-    '';
-  };
-
+  # The final paper, with all graphs, etc.
   paper = render {
     inherit article;
     inherit (graphs) graphs;
@@ -69,28 +32,64 @@ rec {
     '';
   };
 
-  render = { article, graphs, name, script }:
-    runCommand name
+  render = { article, graphs, name, script }: runCommand name
+    {
+      inherit article bibtex graphs latex;
+      buildInputs = [
+        tex
+        unzip
+        (mkBin {
+          name   = "render";
+          script = ''
+            #!/usr/bin/env bash
+            exec pdflatex -interaction=nonstopmode \
+                          -halt-on-error \
+                          --shell-escape article
+          '';
+        })
+      ];
+    }
+    ''
+      set -e
+      unzip "$latex"
+      cp "$article" ./article.tex
+      ${script}
+    '';
+
+  # Provides a pdflatex binary with all packages needed by template, our
+  # document and the matplotlib graphs
+  tex = (texlive.combine {
+    inherit (texlive)
+    scheme-small tikzinclude tikz-qtree algorithmicx algorithm2e algorithms
+    frankenstein csquotes multirow type1cm;
+  });
+
+  # Render a "dummy" version of the paper which has all of the same styling, but
+  # just spits out the text width to stdout. We can use this to generate figures
+  # of the correct size, without having to scale things up or down.
+  textWidth = render {
+    article = writeScript "article-width.tex" (import (runCommand "widthTex.nix"
       {
-        inherit article bibtex graphs latex;
-        buildInputs = [
-          tex
-          unzip
-          (mkBin {
-            name   = "render";
-            script = ''
-              #!/usr/bin/env bash
-              exec pdflatex -interaction=nonstopmode \
-                            -halt-on-error \
-                            --shell-escape article
-            '';
-          })
-        ];
+        inherit article;
+        buildInputs = [ jq ];
       }
       ''
-        set -e
-        unzip "$latex"
-        cp "$article" ./article.tex
-        ${script}
-      '';
+        PREAMBLE=$(grep -B 1000 '\\begin{document}' < "$article")
+
+        echo "$PREAMBLE"                            >  ./article.tex
+        echo '\typeout{WIDTH \the\textwidth WIDTH}' >> ./article.tex
+        echo '\end{document}'                       >> ./article.tex
+
+        jq -Rs '.' < ./article.tex > "$out"
+      ''));
+    graphs  = null;
+    name    = "text-width";
+    script  = ''
+      set -o pipefail
+      render | tee ./output
+      grep 'WIDTH[ ]*[0-9.]*pt[ ]*WIDTH' < ./output |
+      sed -e 's/WIDTH//g'                           |
+      tr -d 'pt ' > "$out"
+    '';
+  };
 }
