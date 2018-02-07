a0890fcec3446d462d5b1f6e602d4a13f0ea3fe4
Chris Warburton <chriswarbo@gmail.com>
2018-02-07 16:12:18 +0000

    Move Bucketing build commands from Makefile to default.nix

--- Bucketing/default.nix
+++ Bucketing/default.nix
@@ -1,13 +1,39 @@
-with import <nixpkgs> {};
+with builtins;
+with { inherit (import ../resources) bibtex nixpkgs styles; };
+with nixpkgs.repo1703."00ef7f9";
 
-stdenv.mkDerivation {
-  name = "paar16";
-  src  = ./.;
-  buildInputs = [
-    bash
-    gnumake
-    (texlive.combine {
-      inherit (texlive) scheme-small tikzinclude tikz-qtree algorithmicx algorithm2e algorithms;
-    })
-  ];
-}
+runCommand "bucketing.pdf"
+  {
+    inherit bibtex;
+    cmd         = ''pdflatex -interaction=nonstopmode \
+                             -halt-on-error --shell-escape report'';
+    src         = ./.;
+    styles      = attrValues styles;
+    buildInputs = [
+      bash
+      gnumake
+      (texlive.combine {
+        inherit (texlive) scheme-small tikzinclude tikz-qtree algorithmicx algorithm2e algorithms;
+      })
+    ];
+  }
+  ''
+    cp "$src"      ./src
+    chmod -r +w    ./src
+    cp "$bibtex"   ./src/Bibtex.bib
+
+    for STYLE in $styles
+    do
+      cp "$STYLE" ./src
+    done
+
+    cd ./src
+
+    $cmd
+    echo "RUNNING bibtex"
+	  bibtex report
+    $cmd
+    $cmd
+
+    cp "report.pdf" "$out"
+  ''
