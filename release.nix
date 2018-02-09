93472406c51fa3b4c0c1ae8e39b75ae6a17afe54
Chris Warburton <chriswarbo@gmail.com>
2018-02-09 17:41:11 +0000

    Include Chalmers presentation in release.nix

--- release.nix
+++ release.nix
@@ -1,11 +1,12 @@
 {
-  AI4FM             = import ./AI4FM;
-  Bucketing         = import ./Bucketing;
-  CICM-2017-report  = import ./CICM-2017-report;
-  JianguoMeeting    = import ./JianguoMeeting;
-  ML4HSTechReport   = import ./ML4HSTechReport;
-  PhDSymposium2017  = import ./PhDSymposium2017;
-  TEBenchmarkPaper  = import ./TEBenchmarkPaper;
-  TheoryExploration = import ./TheoryExploration;
-  TransferReport    = import ./TransferReport;
+  AI4FM              = import ./AI4FM;
+  Bucketing          = import ./Bucketing;
+  Chalmers2015-03-25 = import ./Chalmers2015-03-25;
+  CICM-2017-report   = import ./CICM-2017-report;
+  JianguoMeeting     = import ./JianguoMeeting;
+  ML4HSTechReport    = import ./ML4HSTechReport;
+  PhDSymposium2017   = import ./PhDSymposium2017;
+  TEBenchmarkPaper   = import ./TEBenchmarkPaper;
+  TheoryExploration  = import ./TheoryExploration;
+  TransferReport     = import ./TransferReport;
 }
