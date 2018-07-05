8c6eb44013f80f05ec2e060706a20abfde105868
Chris Warburton <chriswarbo@gmail.com>
2018-07-05 17:04:51 +0100

    trigger-build documentation

--- trigger-build.sh
+++ trigger-build.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
-# Trigger a build on Laminar continuous integration server. Useful as a git hook
-# for both post-commit (to check that local changes work) and post-receive (to
-# perform the "real" build).
+set -e
+
+# Trigger a build on Laminar continuous integration server.
+
+# This can be useful as a post-commit hook for working copies, to check that
+# local changes build correctly. It could also be used as a post-receive hook on
+# a central repository, to perform the "real" build, but warbo-utilities has a
+# more generic post-receive.hook.remote which can do the job better.
 if command -v laminarc > /dev/null
 then
   echo "Queueing laminar build" 1>&2
