7fc964808f12c644dd41623894ef9a43fb661d0c
Chris Warburton <chriswarbo@gmail.com>
2018-07-05 22:28:25 +0100

    Add warbo-packages to resources/

--- resources/default.nix
+++ resources/default.nix
@@ -2,17 +2,24 @@
 with builtins;
 with rec {
   # Allow each project to mix and match the particular versions they want of
-  # nixpkgs and nix-config (if any). This ensures they remain reproducible.
+  # nixpkgs, nix-config, etc. This ensures they remain reproducible.
+
+  repoSouce = http://chriswarbo.net/git;
 
   # Load nixpkg versions with and without custom overrides
   nixpkgs-with-config = nixpkgs: config: import nixpkgs {
     config = import "${config}/config.nix";
   };
-  nixpkgs-without-config = nixpkgs: import nixpkgs { config = {}; };
+  nixpkgs-without-config = version: nixpkgs:
+    import nixpkgs (if compareVersions version "repo1703" == -1
+                       then { config = {}; }
+                       else { config = {}; overlays = []; });
 
   # Whichever nixpkgs the system is using. Only use this to get fixed output
   # derivations, otherwise we risk breaking reproducibility.
-  unstable-nixpkgs = nixpkgs-without-config <nixpkgs>;
+  unstable-nixpkgs = nixpkgs-without-config
+    "repo${(import <nixpkgs> {}).lib.nixpkgsVersion}"
+    <nixpkgs>;
 
   # Arbitrary, but known, versions of nixpkgs and nix-config, used as a base
   stable-nixpkgs-src = unstable-nixpkgs.fetchFromGitHub {
@@ -21,17 +28,16 @@ with rec {
     rev    = "f22817d";
     sha256 = "1cx5cfsp4iiwq8921c15chn1mhjgzydvhdcmrvjmqzinxyz71bzh";
   };
-  stable-nixpkgs = nixpkgs-without-config stable-nixpkgs-src;
 
   # A stable package set which includes some overrides, for when we need them
   stable-configured = nixpkgs-with-config stable-nixpkgs-src
                                           nix-configs."809056c";
 
-  inherit (stable-nixpkgs.lib) mapAttrs;
+  inherit (unstable-nixpkgs.lib) mapAttrs;
 
   # Particular versions of our custom nix-config overrides
-  nix-config-url = http://chriswarbo.net/git/nix-config.git;
-  nix-configs    = mapAttrs (rev: sha256: stable-nixpkgs.fetchgit {
+  nix-config-url = "${repoSouce}/nix-config.git";
+  nix-configs    = mapAttrs (rev: sha256: unstable-nixpkgs.fetchgit {
                                             inherit rev sha256;
                                             url = nix-config-url;
                                           }) {
@@ -49,15 +55,25 @@ with rec {
 
   # All combinations of nixpkgs and nix-config versions
   nixpkgs =
-    with stable-nixpkgs.lib;
+    with unstable-nixpkgs.lib;
     mapAttrs
-      (_: nixpkgs: { configless = nixpkgs-without-config nixpkgs; } //
-                   mapAttrs (_: nixpkgs-with-config nixpkgs)
-                            nix-configs)
+      (version: nixpkgs:
+        { configless = nixpkgs-without-config version nixpkgs; } //
+        mapAttrs (_: nixpkgs-with-config nixpkgs)
+                 nix-configs)
       nixpkgs-repos;
+
+  warbo-packages = mapAttrs
+    (rev: sha256: import (unstable-nixpkgs.fetchgit {
+      inherit rev sha256;
+      url = "${repoSouce}/warbo-packages.git";
+    }))
+    {
+      "c2ea27d" = "04aif1s3cxk27nybsxp571fmvswy5vbw0prq67y108sb49mm3288";
+    };
 };
 rec {
-  inherit nixpkgs;
+  inherit nixpkgs warbo-packages;
   bibtex = ../Bibtex.bib;
   styles = stable-configured.dirsToAttrs ./styles;
 }
