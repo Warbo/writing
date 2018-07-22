a76944d947117326e018517647655396d764488b
Chris Warburton <chriswarbo@gmail.com>
2018-07-22 17:09:11 +0100

    Update Balmoral2015 build script

--- Balmoral2015/default.nix
+++ Balmoral2015/default.nix
@@ -1,9 +1,12 @@
-with import <nixpkgs> {};
-
-stdenv.mkDerivation {
+with import ../resources;
+with {
+  pkgs = nixpkgs.repo1609.configless;
+  ours = warbo-packages."c2ea27d";
+};
+pkgs.stdenv.mkDerivation {
   name = "balmoral-2015";
   buildInputs = [
-    haskell.packages.ghc784.Agda
-    md2pdf
+    pkgs.haskell.packages.ghc784.Agda
+    ours.md2pdf
   ];
 }
