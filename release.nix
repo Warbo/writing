d35cda9950a3c3e649b6fc7673ddb2c92ece986b
Chris Warburton <chriswarbo@gmail.com>
2018-07-22 17:09:31 +0100

    Add more documents to release.nix

--- release.nix
+++ release.nix
@@ -1,9 +1,11 @@
 {
   AI4FM              = import ./AI4FM;
+  Balmoral2015       = import ./Balmoral2015;
   Bucketing          = import ./Bucketing;
   Chalmers2015-03-25 = import ./Chalmers2015-03-25;
   CICM-2017-report   = import ./CICM-2017-report;
   JianguoMeeting     = import ./JianguoMeeting;
+  ML4HS1             = import ./ML4HS1;
   ML4HSTechReport    = import ./ML4HSTechReport;
   PhDSymposium2017   = import ./PhDSymposium2017;
   PhDThesis          = import ./PhDThesis;
