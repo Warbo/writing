8b64c54bcb38967f1019e2e82c31c25a82037280
Chris Warburton <chriswarbo@gmail.com>
2018-09-22 16:25:32 +0100

    Graph is of bucket count not size

--- Bucketing/supporting-material/default.nix
+++ Bucketing/supporting-material/default.nix
@@ -9,10 +9,10 @@ rec {
       extract     = ''
         to_entries | .[] | .key as $size | .value |
           to_entries | .[] | .key as $method | .value |
-            to_entries | .[] | .key as $bucketSize | .value | .proportion |
+            to_entries | .[] | .key as $bucketCount | .value | .proportion |
               [$method,
                $size,
-               $bucketSize,
+               $bucketCount,
                (.mean   | tostring),
                (.stddev | tostring)
                ] | join("\t")
@@ -24,7 +24,9 @@ rec {
       script = writeScript "bucketing-graph.gnuplot" ''
         set terminal pngcairo enhanced font "arial,10" fontscale 1.0 size 600, 400
         set output 'hashed.png'
-        set title "Attempt 1"
+        set title "Bucketing Cost, Recurrent vs Pseudorandom"
+        set xlabel "Number of Functions"
+        set ylabel "Available Proportion of Ground Truth"
         set yrange [0:1]
         set ytics scale 0.1
         set xtics scale 1
@@ -47,22 +49,22 @@ rec {
         METHODS+=("$METHOD")
       done < <(cut -f1 < rows.tsv | sort -u)
 
-      echo "Finding bucket sizes" 1>&2
+      echo "Finding bucket counts" 1>&2
       # Use 'sort' to do two things: strip out dupes ('-u'), and put in
       # descending order ('-r') so that entry 0 is the largest (-n is numeric)
-      BUCKETSIZES=()
-      while read -r BUCKETSIZE
+      BUCKETCOUNTS=()
+      while read -r BUCKETCOUNT
       do
-        BUCKETSIZES+=("$BUCKETSIZE")
-      done < <(cut -f3 < rows.tsv | sort -u -n -r)
+        BUCKETCOUNTS+=("$BUCKETCOUNT")
+        MAXBUCKET="$BUCKETCOUNT"  # After the loop, this will be max bucket count
+      done < <(cut -f3 < rows.tsv | sort -u -n)
 
       MINGREY=0
       MAXGREY=200
-      MAXBUCKET="''${BUCKETSIZES[0]}"
       GREYSTEP=$(( (MAXGREY - MINGREY) / MAXBUCKET ))
       echo "Using greyscale step $GREYSTEP (largest bucket is $MAXBUCKET)" 1>&2
       unset MAXBUCKET
-      unset MAXGREY
+      unset MINGREY
 
       echo "Extracting data" 1>&2
       PLOTS=()
@@ -71,11 +73,11 @@ rec {
         # Recurrent is solid, hashed is dashed
         DASHTYPE=""
         [[ "x$METHOD" = "xhashed" ]] && DASHTYPE='dashtype "- "'
-        for BUCKETSIZE in "''${BUCKETSIZES[@]}"
+        for BUCKETCOUNT in "''${BUCKETCOUNTS[@]}"
         do
-          F="bucketsize_''${BUCKETSIZE}_method_''${METHOD}.tsv"
+          F="bucketcount_''${BUCKETCOUNT}_method_''${METHOD}.tsv"
 
-          GREY=$(( (GREYSTEP * BUCKETSIZE) + MINGREY ))
+          GREY=$(( MAXGREY - (GREYSTEP * BUCKETCOUNT) ))
           HEX=$(printf '%02X' "$GREY")
           echo "GREY $GREY HEX $HEX"
           unset GREY
@@ -83,14 +85,14 @@ rec {
           PLOTS+=("${concatStringsSep " " [
                        ''\"$F\"''
                        ''using 1:2''
-                       ''title \"$METHOD bucket size $BUCKETSIZE\"''
+                       ''title \"$METHOD bucket count $BUCKETcount\"''
                        ''with lines''
                        ''$DASHTYPE''
                        ''linetype rgb \"#$HEX$HEX$HEX\"''
                    ]}")
 
-          echo "Extracting bucket size $BUCKETSIZE for method $METHOD" 1>&2
-          grep "^$METHOD	[0-9]*	$BUCKETSIZE	" < rows.tsv | cut -f 2,4,5 |
+          echo "Extracting bucket count $BUCKETCOUNT for method $METHOD" 1>&2
+          grep "^$METHOD	[0-9]*	$BUCKETCOUNT	" < rows.tsv | cut -f 2,4,5 |
                                                              sort -n >> "$F"
         done
       done
