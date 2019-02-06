1d1e2e945aa2c15a00739504d0596a824164c306
Chris Warburton <chriswarbo@gmail.com>
2019-02-06 19:36:38 +0000

    Make bucketing paper release depend on tests

--- Bucketing/release.nix
+++ Bucketing/release.nix
@@ -1,3 +1,8 @@
-import ./default.nix {
+with rec {
   nixpkgs = (import ../resources).nixpkgs.repo1703."00ef7f9";
-}
+
+  results = import ./default.nix { inherit nixpkgs; };
+
+  inherit (results.nix-helpers) allDrvsIn withDeps;
+};
+withDeps (allDrvsIn results.checks) results.paper
