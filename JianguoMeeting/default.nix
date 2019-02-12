d574730a2cd220247b0b27379378a54de5eeaa51
Chris Warburton <chriswarbo@gmail.com>
2019-02-12 10:46:56 +0000

    Bump deps

--- JianguoMeeting/default.nix
+++ JianguoMeeting/default.nix
@@ -1,8 +1,8 @@
-with { inherit (import ../resources) nixpkgs warbo-packages; };
-with nixpkgs.repo1703."2cc683b";
+with (import ../resources).nixpkgs-joined;
+
 runCommand "render"
   {
-    buildInputs    = [ goat warbo-packages."c2ea27d".pandocPkgs ];
+    buildInputs    = [ goat pandocPkgs ];
     src            = ./.;
     LANG           = "en_US.UTF-8";
     LOCALE_ARCHIVE = "${glibcLocales}/lib/locale/locale-archive";
