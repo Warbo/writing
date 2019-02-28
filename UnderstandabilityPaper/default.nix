214b4e05e8c2859f7c84765c7202ba790fec7d07
Chris Warburton <chriswarbo@gmail.com>
2019-02-28 14:36:40 +0000

    Generate a zip file of our contribution

--- UnderstandabilityPaper/default.nix
+++ UnderstandabilityPaper/default.nix
@@ -1,24 +1,96 @@
-with { inherit (import ../resources) bibtex nixpkgs-joined; };
-with nixpkgs-joined;
-runCommand "machinelearning.pdf"
-  {
-    inherit bibtex;
-    buildInputs = [ (texlive.combine { inherit (texlive) scheme-small; }) ];
-    f           = ./machinelearning.tex;
-  }
-  ''
-    ln -s "$f"      machinelearning.tex
-    ln -s "$bibtex" Bibtex.bib
+with { resources = import ../resources; };
+with resources.nixpkgs-joined;
+with rec {
+  tex = texlive.combine { inherit (texlive) scheme-small; };
 
-    function go {
-      echo "Running pdflatex" 1>&2
-      pdflatex machinelearning
+  src = ./machinelearning.tex;
+
+  # Narrow down our huge Bibtex file to just the things we reference
+  bib = runCommand "bibtex.bib"
+    {
+      inherit src;
+      inherit (resources) bibtex;
+      buildInputs = [ bibtool tex ];
     }
+    ''
+      ln -s "$src"    article.tex
+      ln -s "$bibtex" bibtex.bib
 
-    go
-    go
-    #echo "Running bibtex" 1>&2
-    #bibtex machinelearning
-    go
-    mv machinelearning.pdf "$out"
-  ''
+      pdflatex article
+
+      bibtool -x article.aux -o NewBib.bib
+
+      mv NewBib.bib "$out"
+    '';
+
+  # The main rendered output
+  pdf = runCommand "article.pdf"
+    {
+      inherit bib src;
+      buildInputs = [ tex ];
+    }
+    ''
+      ln -s "$src" article.tex
+      ln -s "$bib" bibtex.bib
+
+      function go {
+        echo "Running pdflatex" 1>&2
+        pdflatex article
+      }
+
+      go
+      go
+      #echo "Running bibtex" 1>&2
+      #bibtex article
+      go
+      mv article.pdf "$out"
+    '';
+
+  dir = {
+    "article.tex" = src;
+    "article.pdf" = pdf;
+    "bibtex.bib"  = bib;
+  };
+
+  archive = runCommand "article.zip"
+    {
+      content     = attrsToDirs' "article" dir;
+      buildInputs = [ zip ];
+    }
+    ''
+      # Chase down symlink targets
+      cp -rL "$content" article
+
+      # Zip up the actual files, rather than symlinks
+      zip -r "$out" article
+    '';
+
+  check = runCommand "article-check"
+    {
+      inherit archive;
+      buildInputs = [ fail unzip ];
+    }
+    ''
+      unzip "$archive"
+      [[ -d article ]] || {
+        find . 1>&2
+        fail "Didn't make 'article' dir"
+      }
+
+      for F in article.tex article.pdf bibtex.bib
+      do
+        [[ -f article/"$F" ]] || {
+          find .
+          fail "Didn't make 'article/$F'"
+        }
+      done
+
+      echo pass > "$out"
+    '';
+
+  checked = withDeps [ check ] archive;
+};
+
+attrsToDirs' "machine-learning-section" (dir // {
+  "article.zip" = checked;
+})
