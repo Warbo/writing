72a307c839763eeb55f086b2443d433ad5846c35
Chris Warburton <chriswarbo@gmail.com>
2019-04-11 13:48:51 +0100

    Use nixpkgs-joined to collate styles

--- resources/default.nix
+++ resources/default.nix
@@ -1,23 +1,21 @@
 # Resources used by multiple projects, including particular versions of nixpkgs
 with builtins;
 with rec {
-  # Allow each project to mix and match the particular versions they want of
-  # nixpkgs, nix-config, etc. This ensures they remain reproducible.
+  # Pinned versions of nixpkgs, nix-helpers, etc. which each project can use to
+  # ensures they remain reproducible.
 
   repoSouce = http://chriswarbo.net/git;
 
-  # Load nixpkg versions with and without custom overrides
-  nixpkgs-with-config = nixpkgs: config: import nixpkgs {
-    config = import "${config}/config.nix";
-  };
-
   # Use the system's nixpkgs to fetch git repos (since they're fixed-output,
-  # this shouldn't affect reproducibility).
-  inherit (with { unstableVersion = (import <nixpkgs> {}).lib.nixpkgsVersion; };
-           import <nixpkgs> (if compareVersions unstableVersion "1703" == -1
-                                then { config = {}; }
-                                else { config = {}; overlays = []; }))
-    fetchFromGitHub fetchgit;
+  # this shouldn't affect reproducibility). cleanSlate avoids user overrides.
+  inherit (import <nixpkgs> cleanSlate) fetchFromGitHub fetchgit;
+
+  # We don't know what version <nixpkgs> might be, so we check whether it's too
+  # old for the 'overlays' argument, to avoid triggering an error.
+  cleanSlate = if -1 == compareVersions (import <nixpkgs> {}).lib.nixpkgsVersion
+                                        "1703"
+                  then { config = {}; }
+                  else { config = {}; overlays = []; };
 
   # Arbitrary, but known, version of nixpkgs to use as a base
   stable-nixpkgs-src = fetchFromGitHub {
@@ -28,7 +26,9 @@ with rec {
   };
 
   # A stable package set which includes some overrides, for when we need them
-  stable-configured = nixpkgs-with-config stable-nixpkgs-src nix-config;
+  stable-configured = import stable-nixpkgs-src {
+    config = import "${nix-config}/config.nix";
+  };
 
   # Particular versions of our custom nix-config overrides
   nix-config = fetchgit {
@@ -56,5 +56,5 @@ with rec {
 rec {
   inherit nixpkgs-joined;
   bibtex = ../Bibtex.bib;
-  styles = stable-configured.dirsToAttrs ./styles;
+  styles = nixpkgs-joined.dirsToAttrs ./styles;
 }
