04c5f5949db6d8d84b9e905f0d9210fb6b332843
Chris Warburton <chriswarbo@gmail.com>
2019-04-11 14:51:33 +0100

    Some inlining and commenting

--- resources/default.nix
+++ resources/default.nix
@@ -1,11 +1,7 @@
-# Resources used by multiple projects, including particular versions of nixpkgs
+# Resources used by multiple projects, including pinned versions of nixpkgs,
+# nix-helpers, etc. to ensure reproducibility.
 with builtins;
 with rec {
-  # Pinned versions of nixpkgs, nix-helpers, etc. which each project can use to
-  # ensures they remain reproducible.
-
-  repoSouce = http://chriswarbo.net/git;
-
   # Use the system's nixpkgs to fetch git repos (since they're fixed-output,
   # this shouldn't affect reproducibility). cleanSlate avoids user overrides.
   inherit (import <nixpkgs> cleanSlate) fetchFromGitHub fetchgit;
@@ -17,31 +13,37 @@ with rec {
                   then { config = {}; }
                   else { config = {}; overlays = []; };
 
-  # Arbitrary, but known, version of nixpkgs to use as a base
-  stable-nixpkgs-src = fetchFromGitHub {
-    owner  = "NixOS";
-    repo   = "nixpkgs";
-    rev    = "6a3f5bc";
-    sha256 = "1ib96has10v5nr6bzf7v8kw7yzww8zanxgw2qi1ll1sbv6kj6zpd";
-  };
+  # Fetch our custom package overlay. This repo also includes a pinned version
+  # of nix-helpers, so we use that (to avoid repeating ourselves).
+  warbo-packages-src =
+    with { repoSouce = http://chriswarbo.net/git; };
+    fetchgit {
+      url    = "${repoSouce}/warbo-packages.git";
+      rev    = "9f129aa";
+      sha256 = "1v35m8xxqav8cq4g1hjn8yhzhaf9g4jyrmz9a26g7hk04ybjwc7k";
+    };
 
-  warbo-packages-src = fetchgit {
-    url    = "${repoSouce}/warbo-packages.git";
-    rev    = "9f129aa";
-    sha256 = "1v35m8xxqav8cq4g1hjn8yhzhaf9g4jyrmz9a26g7hk04ybjwc7k";
-  };
-
+  # Loads an overlay of helper functions for Nix, from the pinned version we
+  # keep in warbo-packages.
   nix-helpers-overlays =
     with { src = (import "${warbo-packages-src}/helpers.nix" {}).nix-helpers; };
     [ (import "${src}/overlay.nix") ];
 
+  # A pinned version of nixpkgs, with nix-helpers and warbo-packages overlays
   nixpkgs-joined =
-    with {
-      # A stable package set which includes some overrides, for when we need them
-      stable-configured = import stable-nixpkgs-src {
-        overlays = nix-helpers-overlays;
+    with rec {
+      # Arbitrary, but known, version of nixpkgs to use as a base
+      stable = fetchFromGitHub {
+        owner  = "NixOS";
+        repo   = "nixpkgs";
+        rev    = "6a3f5bc";
+        sha256 = "1ib96has10v5nr6bzf7v8kw7yzww8zanxgw2qi1ll1sbv6kj6zpd";
       };
+
+      # Overlay with nix-helpers to make repoXXXX definitions available
+      stable-configured = import stable { overlays = nix-helpers-overlays; };
     };
+    # Pick a version of nixpkgs and add both of our overlays to it
     import stable-configured.repo1809 {
       config   = {};
       overlays = nix-helpers-overlays ++ [
