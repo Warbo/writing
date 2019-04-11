d68d28664ea900b396df9686dace85fd48ad01c4
Chris Warburton <chriswarbo@gmail.com>
2019-04-11 15:28:42 +0100

    Get names of samples which failed

--- Bucketing/supporting-material/contents.nix
+++ Bucketing/supporting-material/contents.nix
@@ -59,4 +59,47 @@ rec {
       cd "$out"
       "$script"
     '';
+
+  allFailed = filter (name: (count name).successes == 0) names;
+
+  # Decodes a hex-encoded name like 'global0123456789abcdef'
+  deHex = s:
+    with rec {
+      suffix = substring 6 (stringLength s) s;
+
+      getPairs = x: with { l = stringLength x; };
+                    if l < 2
+                       then []
+                       else [(substring 0 2 x)] ++ getPairs (substring 2 l x);
+
+      pairs = getPairs suffix;
+
+      digits = { "0" = 0 ; "1" = 1 ; "2" = 2 ; "3" = 3; "4" = 4; "5" = 5; "6" = 6;
+                 "7" = 7 ; "8" = 8 ; "9" = 9 ; "a" = 10;  "b" = 11;  "c" = 12;
+                 "d" = 13; "e" = 14; "f" = 15; };
+
+      decode = pair:
+        with rec {
+          high  = getAttr (substring 0 1 pair) digits;
+          low   = getAttr (substring 1 2 pair) digits;
+          n     = (high * 16) + low;
+          str   = toString n;
+          upper = n > 64 && n < 91;
+          lower = n > 96 && n < 123;
+          digit = n > 47 && n < 58;
+          other = { "46" = "."; "47" = "/"; "95" = "_"; };
+        };
+        if upper
+           then elemAt upperChars (n - 65)
+           else if lower
+                   then elemAt lowerChars (n - 97)
+                   else if digit
+                           then toString (n - 48)
+                           else if hasAttr str other
+                                   then getAttr str other
+                                   else abort "Unknown char ${str}";
+
+      chars = map decode pairs;
+    };
+    concatStringsSep "" chars;
 }
